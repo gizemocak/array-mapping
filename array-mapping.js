95a4fd580366d6bc69594b6c2359da32a1730c5f
gizemocak <gizem_ocak@outlook.com>
2019-05-02 17:49:41 +0000

    used map for the assertion to pass

---
+++ array-mapping.js
@@ -0,0 +1,17 @@
+var input = [{
+        x: 3,
+        y: 4
+    },
+    {
+        x: 12,
+        y: 5
+    },
+    {
+        x: 8,
+        y: 15
+    }
+];
+var result = input.map(element => Math.sqrt(element.x ** 2 + element.y ** 2))
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
